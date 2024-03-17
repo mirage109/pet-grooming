5215506bdc30be1a9ca68f4c23bd6955124d4df5
Anastasiia Zibla <zibla.asia@gmail.com>
2024-03-17 17:40:46 +0100

    feat: slider swiper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Hero from "./components/Hero";
+import Header from "./components/Header";
 
 const App = () => {
   return <div className='w-full max-w-[1440px] mx-auto bg-orange-quaternary relative'>
+    <Header/>
     <Hero />
+    <div className='h-[2000px]'></div>
   </div>;
 };
 
