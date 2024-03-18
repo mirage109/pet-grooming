847d50544b2843cab8aefb0828da12410d3a0d2d
Anastasiia Zibla <zibla.asia@gmail.com>
2024-03-18 11:35:27 +0100

    feat: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,16 +21,19 @@ const Header = () => {
         bg ? "bg-white py-3 shadow-md" : "bg-none py-5"
       } fixed w-full right-0 left-0 z-10 transition-all duration-300`}
     >
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         <a href="#">
-          <img src={Logo} alt="logo"/>
+          <img src={Logo} alt="logo" />
         </a>
-        <div>
-          <Nav/>
-          <Program/>
-          <div>
-            <CgMenuRight/>
-          </div>
+        <div className="hidden lg:flex">
+          <Nav />
+        </div>
+        <Program />
+        <div onClick={()=>setNavMobile(!navMobile)} className="lg:hidden cursor-pointer">
+          <CgMenuRight className="text-blue text-3xl" />
+        </div>
+        <div className={`${navMobile ? 'max-h-[260px]':'max-h-0'} fixed bg-yellow shadow-lg left-0 w-full top-[78px] h-full overflow-hidden transition-all`}>
+          <NavMobile />
         </div>
       </div>
     </header>
