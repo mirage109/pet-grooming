3244da8f9be6889fe1105c513d0dd6d6738c6765
Anastasiia Zibla <zibla.asia@gmail.com>
2024-03-18 17:38:16 +0100

    feat: prices

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Appointment from "./components/Appointment";
+import Prices from "./components/Prices";
 
 const App = () => {
   return <div className='w-full max-w-[1440px] mx-auto bg-orange-quaternary relative'>
     <Header/>
     <Hero />
     <Appointment />
+    <Prices />
     <div className='h-[2000px]'></div>
   </div>;
 };
--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { bundleData } from '../data';
+import Bundles from './Bundles';
 
 const Prices = () => {
-  return <div>Prices</div>;
+  return <section className='py-12 lg:py-24'>
+    <div className='containe mx-auto'>
+      <div className='text-center mb-20'>
+        <div className='text-orange font-semibold mb-3'>Our Prices</div>
+        <h2 className='h2 mb-3'>How Big Your Dog Is?</h2>
+        <p className='text-lg text-blue'>Choose your dog category</p>
+      </div>
+      <div></div>
+    </div>
+  </section>;
 };
 
 export default Prices;
