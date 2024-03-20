4bb8c62f57842fc161a91c295c07ac0f01e2a545
Anastasiia Zibla <zibla.asia@gmail.com>
2024-03-20 12:12:27 +0100

    feat:contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Appointment from "./components/Appointment";
 import Prices from "./components/Prices";
+import Contact from "./components/Contact";
 
 const App = () => {
   return <div className='w-full max-w-[1440px] mx-auto bg-orange-quaternary relative'>
@@ -10,7 +11,7 @@ const App = () => {
     <Hero />
     <Appointment />
     <Prices />
-    <div className='h-[2000px]'></div>
+    <Contact/>
   </div>;
 };
 
