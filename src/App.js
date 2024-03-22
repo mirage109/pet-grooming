5a6d9293ee6dc6404635a842b9d8f22dac64634d
Anastasiia Zibla <zibla.asia@gmail.com>
2024-03-22 11:28:27 +0100

    feat: footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Appointment from "./components/Appointment";
 import Prices from "./components/Prices";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return <div className='w-full max-w-[1440px] mx-auto bg-orange-quaternary relative'>
@@ -12,6 +13,7 @@ const App = () => {
     <Appointment />
     <Prices />
     <Contact/>
+    <Footer/>
   </div>;
 };
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import Logo from "../assets/img/logo-white.svg";
+import { social } from "../data";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-orange py-8 lg:pu-4">
+      <div className="container mx-auto">
+        <div className="flex text-white flex-col gap-y-6 lg:flex-row items-center justify-between">
+          <img src={Logo} alt="logo" />
+          <div className="text-[15px]">&copy; 2022 All rights reserved</div>
+          <div className="flex gap-x-4">
+            {social.map((item, index) => {
+              return (
+                <a key={index} href={item.href}>
+                  <div className="bg-[#fe8d71a9] hover:bg-[#fe8d71] w-10 h-10 rounded-full flex justify-center items-center transition text-2xl ">
+                    {item.icon}
+                  </div>
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
